c6d599de78d69e762a35a59a8f71ab3e95cc75a9
amitkumar8636 <amitkumar8636@gmail.com>
2019-09-17 17:41:42 +0530

    CookBook Initialized

---
+++ cookbook/src/main/java/com/practise/cookbook/service/ReportService.java
@@ -0,0 +1,14 @@
+package com.practise.cookbook.service;
+
+/**
+ * 
+ * @author Team1
+ * @viewedBy certainSomeone
+ * Service Interface ReportService
+ *
+ */
+public interface ReportService {
+
+	public String getReport();
+
+}
